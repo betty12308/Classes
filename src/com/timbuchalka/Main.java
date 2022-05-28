a8be6af11ad36826162c3a84a7e88bfed80ce79b
betty_guo22 <elizabethpguo@gmail.com>
2022-05-27 22:24:19 -0400

    Initial commit

---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,11 @@
+package com.timbuchalka;
+
+public class Main {
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car Lexus = new Car();
+        System.out.println("Model is " + porsche.getModel());
+        porsche.setModel("carrera");
+        System.out.println("Model is " + porsche.getModel());
+    }
+}
